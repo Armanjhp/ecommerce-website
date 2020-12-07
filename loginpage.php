91cb9167266d72db682400c39f7b6f18c607a821
Dwarf1er <poulin.antoine@hotmail.com>
2020-12-06 22:59:30 -0500

    Separated login and register pages

--- loginpage.php
+++ loginpage.php
@@ -12,53 +12,32 @@
     require_once "database.php";
     $username = "";
     $password = "";
-    $confirm_password = "";
     $username_error = "";
     $password_error = "";
-    $confirm_password_error = "";
 
-    //REGISTER
+    //ALREADY LOGGED IN
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        header("location: about.php");
+        exit();
+    }
+
+    //LOG IN
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        //Username validation
         if(empty(trim($_POST["username"]))) {
-            $username_error = "Please enter a username";
+            $username_error = "Please enter your username";
             echo '<script language="javascript">';
-            echo 'alert("Please enter a username")';
+            echo 'alert("Pleaser enter your username")';
             echo '</script>';
         }
 
         else {
-            $sql_query = "SELECT id FROM users WHERE username = ?";
-
-            if($stmt = mysqli_prepare($db,$sql_query)) {
-                mysqli_stmt_bind_param($stmt, "s", $param_username);
-                $param_username = trim($_POST["username"]);
-
-                if(mysqli_stmt_execute($stmt)) {
-                    mysqli_stmt_store_result($stmt);
-                    if(mysqli_stmt_num_rows($stmt) == 1) {
-                        $username_error = "This username is already taken";
-                        echo '<script language="javascript">';
-                        echo 'alert("This username is already taken")';
-                        echo '</script>';
-                    }
-    
-                    else {
-                        $username = trim($_POST["username"]);
-                    }
-                }
-                mysqli_stmt_close($stmt);
-            }
+            $username = trim($_POST["username"]);
         }
 
         if(empty(trim($_POST["password"]))) {
-            $password_error = "Please enter a password";
-        }
-
-        elseif(strlen(trim($_POST["password"])) < 8) {
-            $password_error = "Password must be at least 8 characters long";
+            $password_error = "Pleaser enter your password";
             echo '<script language="javascript">';
-            echo 'alert("Password must be at least 8 characters long")';
+            echo 'alert("Pleaser enter your password")';
             echo '</script>';
         }
 
@@ -66,30 +45,41 @@
             $password = trim($_POST["password"]);
         }
 
-        if(empty(trim($_POST["confirm_password"]))) {
-            $confirm_password_error = "Confirm your password";
-        }
+        if(empty($username_error) && empty($password_error)) {
+            $sql_query = "SELECT id, username, password, FROM chromerce.users WHERE username = ?";
 
-        else {
-            $confirm_password = trim($_POST["confirm_password"]);
-            if(empty($password_error) || ($password != $confirm_password)) {
-                $confirm_password = "Passowrds must match";
-                echo '<script language="javascript">';
-                echo 'alert("Passwords must match")';
-                echo '</script>';
-            }
-        }
-
-        if(empty($username_error) && empty($password_error) && empty($confirm_password_error)) {
-            $sql_query = "INSERT INTO chromerce.users (username, password) VALUES (?, ?)";
-
-            if ($stmt = mysqli_prepare($db, $sql_query)) {
-                mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_passowrd);
+            if($stmt = mysqli_prepare($db, $sql_query)) {
+                mysqli_stmt_bind_param($stmt, "s", $param_username);
                 $param_username = $username;
-                $param_passowrd = password_hash($password, PASSWORD_DEFAULT);
-
                 if(mysqli_stmt_execute($stmt)) {
-                    header("Location: index.php");
+                    mysqli_stmt_store_result($stmt);
+                    if(mysqli_stmt_num_rows($stmt) == 1) {
+                        mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                        if(mysqli_stmt_fetch($stmt)) {
+                            if(password_verify($password, $hashed_password)) {
+                                session_start();
+                                $_SESSION["loggedin"] = true;
+                                $_SESSION["id"] = $id;
+                                $_SESSION["username"] = $username;
+
+                                header("Location: about.php");
+                            }
+
+                            else {
+                                $password_error = "Password invalid";
+                                echo '<script language="javascript">';
+                                echo 'alert("Password invalid")';
+                                echo '</script>';
+                            }
+                        }
+                    }
+
+                    else {
+                        $username_error = "No account has been found with that username";
+                        echo '<script language="javascript">';
+                        echo 'alert("No account has been found with that username")';
+                        echo '</script>';
+                    }
                 }
 
                 else {
@@ -99,27 +89,14 @@
                 mysqli_stmt_close($stmt);
             }
         }
-        mysqli_close($db);
+
+            mysqli_close($db);
     }
 ?>
     <!---------- navbar ----------->
-    <div class="navbar-container">
-        <nav>
-            <a href="index.html" class="logo"><img src="login-images/logo.png" alt="Chromerce logo" height="250" width="250"></a>
-            <input type="checkbox" id="toggle-navbar" value="2">
-            <label for="toggle-navbar" class="hamburger">
-                <img src="login-images/hamburger.png" alt="hamburger icon">
-            </label>
-            <div class="menu">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-                <a href="#">Account</a>
-                <a href="#">Product</a>
-                <a href="#"><img class="cart" src="login-images/cart.png" alt="Cart image" height="30" width="30" /></a>
-            </div>
-        </nav>
-    </div>
+    <?php 
+        include("navbar.php");
+    ?>
     <!---------- login ---------->
     <div class="login">
         <div class="container">
@@ -128,30 +105,13 @@
                     <div class="form-container">
                         <div class="form-btn">
                             <span onclick="login()">Login</span>
-                            <span onclick="register()">Register</span>
                             <hr id="indicator">
                         </div>
 
-                        <form id="loginForm" action="<?php echo htmlspecialchars($_SERVER["login.php"]); ?>" method="post">
-                            <input type="text" id="loginname" name="loginname" placeholder="Username">
-                            <input type="password" id="loginpassword" name="loginpassword" placeholder="Password">
+                        <form id="loginForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                            <input type="text" id="username" name="username" placeholder="Username">
+                            <input type="password" id="password" name="password" placeholder="Password">
                             <button type="submit" class="btn">Login</button>
-                        </form>
-
-                        <form id="registerForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                            <div class="<?php echo (!empty($username_error)) ? 'has-error' : ''; ?>">
-                                <input type="text" id="username" name="username" placeholder="Username" value="<?php echo $username; ?>">
-                            </div>
-                            <div class="passwordinfo <?php echo (!empty($password_error)) ? 'has-error' : ''; ?>">
-                                <input type="password" id="password" name="password" placeholder="Password" value="<?php echo $password; ?>">
-                                <div class="info">&#x1F6C8;
-                                    <span class="infotext">Password must be 8 characters long</span>
-                                </div>
-                            </div>
-                            <div class="<?php echo (!empty($confirm_password_error)) ? 'has-error' : ''; ?>">
-                            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" value="<?php echo $confirm_password; ?>">
-                            </div>
-                            <button type="submit" class="btn">Register</button>
                         </form>
                     </div>
                 </div>
@@ -160,60 +120,7 @@
     </div>
 
 	<!---------- footer ---------->
-
-	<div class="footer">
-		<div class="container">
-			<div class="row">
-				<div class="footer-col-1">
-					<h3>Download Our App</h3>
-					<p>Download App for Android and ios devices</p>
-					<div class="app-logo">
-						<a href="https://play.google.com/">
-							<img src="about-images/play-store.png" alt="Google Play Store logo">
-						</a>
-						<a href="https://www.apple.com/shop">
-							<img src="about-images/app-store.png" alt="Apple Store Logo">
-						</a>
-					</div>
-				</div>
-				<div class="footer-col-2">
-					<h3>Useful links</h3>
-					<ul>
-						<li>Coupons</li>
-						<li>Return Policy</li>
-						<li>Join Affiliate</li>
-					</ul>
-				</div>
-				<div class="footer-col-3">
-					<h3>Follow us</h3>
-					<ul>
-						<li><a href="https://www.facebook.com/">Facebook</a></li>
-						<li><a href="https://twitter.com/">Twitter</a></li>
-						<li><a href="https://instagram.com/">Instagram</a></li>
-					</ul>
-				</div>
-			</div>
-			<hr>
-			<p class="copyright">Copyright 2020 - Chromerce </p>
-		</div>
-	</div>
-
-    <!---------- JS Account ---------->
-    <script>
-        var loginForm = document.getElementById("loginForm");
-        var registerForm = document.getElementById("registerForm");
-        var indicator = document.getElementById("indicator");
-
-        function register() {
-            registerForm.style.transform = "translateX(0px)";
-            loginForm.style.transform = "translateX(0px)";
-            indicator.style.transform = "translateX(60px)";
-        }
-
-        function login() {
-            registerForm.style.transform = "translateX(300px)";
-            loginForm.style.transform = "translateX(300px)";
-            indicator.style.transform = "translateX(-60px)";
-        }
-    </script>
+    <?php 
+        include("footer.php");
+    ?>
 </body>
