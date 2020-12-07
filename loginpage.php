7595491186cf98f61cb1eb662cdedbe0b37f8a17
Dwarf1er <poulin.antoine@hotmail.com>
2020-12-07 00:46:16 -0500

    CSS fixed

--- loginpage.php
+++ loginpage.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chromerce | Login or Register</title>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link rel="icon" type="image/png" href="template-images/favicon.png"/>
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="template.css">
 </head>
--- registerpage.php
+++ registerpage.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chromerce | Login or Register</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="template.css">
 </head>
