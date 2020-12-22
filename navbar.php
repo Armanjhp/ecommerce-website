5d905aa908833b7e3f4abfd291e2ac09859dbcd7
Arman <armanjahanpour7@gmail.com>
2020-12-22 09:51:22 -0500

    Final revision

--- navbar.php
+++ navbar.php
@@ -1,9 +1,9 @@
     <div class="navbar-container">
         <nav>
-            <a href="index.php" class="logo"><img src="template-images/logo.png" alt="Chromerce logo" height="250" width="250"></a>
+            <a href="index.php" class="logo"><img src="images/logo.png" alt="Chromerce logo" height="250" width="250"></a>
             <input type="checkbox" id="toggle-navbar" value="2">
             <label for="toggle-navbar" class="hamburger">
-                <img src="template-images/hamburger.png" alt="hamburger icon">
+                <img src="images/hamburger.png" alt="hamburger icon">
             </label>
             <div class="menu">
                 <a href="index.php">Home</a>
@@ -21,7 +21,7 @@
                     echo '<a href="login.php">Login</a>';
                 }
                 ?>
-                <a href="cart.php"><img class="cart" src="template-images/cart.png" alt="Cart image" height="30" width="30" /></a>
+                <a href="cart.php"><img class="cart" src="images/cart.png" alt="Cart image" height="30" width="30" /></a>
             </div>
         </nav>
     </div>
